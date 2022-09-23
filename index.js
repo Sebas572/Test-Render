4d17da6e9b8bb8b8cf499e70f8b947a51dae1194
Sebastian <sebastian.balebona@gmail.com>
2022-09-23 00:25:04 -0400

    save

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 require('dotenv').config();
 
 app.get('/', (req, res, next) => {
-        console.log('TEST')
+	console.log('TEST');
 	res.send('Hello World');
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(3000, () => {
 	console.log('init server');
 });
